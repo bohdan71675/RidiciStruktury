0a70c01a3eea20df468c865d8555fa7eb553d6c4
bodnarb.04 <bodnarb.04@gmail.com>
2022-09-29 10:26:03 +0200

    Přidejte soubory projektu.

---
+++ RidiciStruktury.cpp
@@ -0,0 +1,77 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    srand((unsigned)time(NULL));
+    char volba;
+    cout << "\nF = For, W = While, D = DO While, K=Konec: ";
+    cin >> volba;
+    while (volba != 'K')
+    {
+        switch (volba)
+        {
+        case 'F':
+        {
+            int n;
+            double soucet = 0;
+            cout << "Zadej cislo n: ";
+            cin >> n;
+
+            for (int i = 0; i < n; i++)
+            {
+                double nc = 10 + (100 - 10) * (double)rand() / (RAND_MAX + 1);
+                soucet += nc;
+                cout << nc << endl;
+            }
+            cout << "Soucet je " << soucet << endl;
+        }
+        break;
+        case 'W':
+        {
+            int n;
+            int i = 0;
+            double soucet = 0;
+            cout << "Zadej cislo n: ";
+            cin >> n;
+
+            while (i < n)
+            {
+                double nc = 10 + (100 - 10) * (double)rand() / (RAND_MAX + 1);
+                soucet += nc;
+                cout << nc << endl;
+
+                ++i;
+            }
+            cout << "Soucet je " << soucet << endl;
+        }
+        case 'D':
+        {
+            int n;
+            int i = 0;
+            double soucet = 0;
+            cout << "Zadej cislo n: ";
+            cin >> n;
+
+            do {
+                double nc = 10 + (100 - 10) * (double)rand() / (RAND_MAX + 1);
+                soucet += nc;
+                cout << nc << endl;
+
+                ++i;
+            } while (i < n);
+            cout << "Soucet je " << soucet << endl;
+        }
+        break;
+        default:
+        {
+            cout << "Nevybrana moznost";
+        }
+        break;
+        }
+        cout << "\nF = For, W = While, D = DO While, K=Konec: ";
+        cin >> volba;
+    }
+}
